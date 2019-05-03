d048375517638548f9caf67823edf1c9aed7e9d1
purplesands <mullenjoe13@gmail.com>
2019-05-02 21:26:24 -0400

    sounds 1-19

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', e=> {
 songForm = document.querySelector('#songForm')
   songForm.addEventListener('submit', e => {
     e.preventDefault()
-    let song = e.target.song.value.split("")
+    let song = e.target.song.value.toLowerCase().split("")
     console.log(song)
 
     for(var i = 0;i < song.length; i++){
@@ -14,41 +14,106 @@ songForm = document.querySelector('#songForm')
         setTimeout(function(){
           console.log(song[k])
             soundCheck(song[k]);
-        }, 750 * (k + 1));
+        }, 3000 * (k + 1));
     }
-
-}
+  }
 )
 
-function soundCheck(ltr){
 
+function soundCheck(ltr){
   let i = 0
+
+  i++
   switch (ltr) {
     case "a":
     aS = new Audio("assets/1.wav", i)
-    i++
     aS.play()
     break;
     case "b":
     bS = new Audio("assets/2.wav", i)
-    i++
     bS.play()
     break;
     case "c":
     cS = new Audio("assets/3.wav", i)
-    i++
     cS.play()
     break;
     case "d":
     dS = new Audio("assets/4.wav", i)
-    i++
     dS.play()
     break;
     case "e":
     eS = new Audio("assets/5.wav", i)
-    i++
     eS.play()
     break;
+    case "f":
+    fS = new Audio("assets/6.wav", i)
+    fS.play()
+    break;
+    case "g":
+    gS = new Audio("assets/7.wav", i)
+    gS.play()
+    break;
+    case "h":
+    hS = new Audio("assets/8.wav", i)
+    hS.play()
+    break;
+    case "i":
+    iS = new Audio("assets/9.wav", i)
+    iS.play()
+    break;
+    case "j":
+    jS  = new Audio("assets/10.wav", i)
+    jS.play()
+    break;
+    case "k":
+    kS  = new Audio("assets/11.wav", i)
+    kS.play()
+    break;
+    case "l":
+    lS  = new Audio("assets/12.wav", i)
+    lS.play()
+    break;
+    case "m":
+    mS  = new Audio("assets/13.wav", i)
+    mS.play()
+    break;
+    case "n":
+    nS  = new Audio("assets/14.wav", i)
+    nS.play()
+    break;
+    case "o":
+    oS  = new Audio("assets/15.wav", i)
+    oS.play()
+    break;
+    case "p":
+    pS  = new Audio("assets/16.wav", i)
+    pS.play()
+    break;
+    case "q":
+    qS  = new Audio("assets/17.wav", i)
+    qS.play()
+    break;
+    case "r":
+    rS  = new Audio("assets/18.wav", i)
+    rS.play()
+    break;
+    case "s":
+    sS  = new Audio("assets/19.wav", i)
+    sS.play()
+    break;
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
 
